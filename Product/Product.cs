5780cc1808c5cee01940c516628c8cec9f93a713
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-09-20 15:30:57 -0400

    Deleted defautl class Added new Product class with properties and default instance of the product.

---
+++ Product/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product
+{
+    public class Product
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Product()
+        {
+            Manufacturer = "No Manufactorer Set";
+            Model = "No Model Set";
+            Price = 0.00M;
+        }
+
+
+    }
+}
